4d5fa24ef9315df9ea3933ed44b9d7c5ffe6de87
Moikapy <warrenckhan@gmail.com>
2024-03-10 19:28:28 -0400

    refactor pongball to use objectentity from modules

--- scenes/pong_ball.gd
+++ scenes/pong_ball.gd
@@ -1,29 +1,28 @@
-extends CharacterBody2D
+extends ObjectEntity2D
 class_name PongBall
 
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
-@onready var _bounce:Bounce = $Bounce
+@onready var bounce = $Bounce
+
 
 var direction_x = [360]
 var direction_y = [31.415, -31.415]
 
 
 func _ready():
-	#direction.shuffle()
+	super._ready()
+	bounce.on_bounce.connect(on_bounce)
 	var vector = {
 		'x':direction_x.pick_random(),
 		'y':direction_y.pick_random()
 	}
 	
 	set_velocity(Vector2(vector.x,vector.y))
+	print(entity_type)
 
 func _physics_process(delta):
 	var collision_info = move_and_collide(velocity * delta)
-	if collision_info:
-		audio_stream_player_2d.play()
-		velocity = _bounce.bounce(collision_info, velocity)
-		if velocity.x < 200 and velocity.x > -200:
-			velocity.x *= 1.25
-		if velocity.y < 200 and velocity.y > -200:
-			velocity.y *= 1.25
+	velocity = bounce.bounce(collision_info, velocity, delta)
 
+func on_bounce():
+	audio_stream_player_2d.play()
