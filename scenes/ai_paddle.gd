face693e61ecdd725454b30da1b5f57a61cb714a
Moikapy <warrenckhan@gmail.com>
2024-03-12 14:56:05 -0400

    add range to ai_paddle for ball tracking

--- scenes/ai_paddle.gd
+++ scenes/ai_paddle.gd
@@ -4,12 +4,14 @@ class_name AIPaddle
 @onready var input_handler = $InputHandler
 @onready var movement_handler = $MovementHandler
 @onready var ball = get_tree().get_first_node_in_group('ball')
-	
+@export_range(600,800) var aggro_range = 750
 	
 func _physics_process(delta):
 	
 	if ball == null:
 		ball = get_tree().get_first_node_in_group('ball')
 	else:
+		if ball.global_position.x < aggro_range:
+			return
 		movement_handler.movement_handler(self, ball.global_position - global_position, delta)
 		move_and_slide()
